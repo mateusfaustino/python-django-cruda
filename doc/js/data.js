f3549718674b21284dd9c3776156f8b0dfdeccec
Mateus Faustino <mateusfaustino2017@gmail.com>
2023-07-31 14:38:28 -0300

    doc

---
+++ doc/js/data.js
@@ -0,0 +1,28 @@
+
+const data = [
+    {
+        id:1,
+        title:'Instalando o sistema',
+        body:`
+            test
+        
+        `
+    },
+    {
+        id:2,
+        title:'Teste 2',
+        body:`
+            test
+        
+        `
+    },
+    {
+        id:3,
+        title:'Teste 3',
+        body:`
+            test
+        
+        `
+    },
+
+]
